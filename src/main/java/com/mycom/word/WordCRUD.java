5d682b023d6afa0f3aaafdc0b07db5f154b9339c
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-09-03 17:38:19 +0900

    Implement add word system

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -1,10 +1,38 @@
 package com.mycom.word;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+    /*
+     * => 난이도(1,2,3) & 새 단어 입력 :1 driveway
+     * 뜻 입력 :차고 진입로
+     * 새 단어가 단어장에 추가되었습니다.
+     */
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
 
     @Override
     public Object add() {
-        return null;
+        System.out.println("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+        // next 만 사용하면 /n가 버퍼에 남아서 다음에 nextLine()함수를 호출할 때 문제가 생긴다
+
+        System.out.println("뜻 입력 : ");
+        String meaning = s.nextLine();
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord() {
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
     }
 
     @Override
--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -1,10 +1,59 @@
 package com.mycom.word;
 
+import java.util.Scanner;
+
 public class WordManager {
     // 실제 word를 관리하는 클래스
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+    /*
+    *** 영단어 마스터 ***
+    *****************
+    1. 모든 단어 보기
+    2. 수준별 단어 보기
+    3. 단어 검색
+    4. 단어 추가
+    5. 단어 수정
+    6. 단어 삭제
+    7. 파일 저장
+    0. 나가기
+    *****************
+    => 원하는 메뉴는?
+     */
 
+    WordManager() {
+        wordCRUD = new WordCRUD(s);
+    }
+
+    public int selectMenu() {
+        System.out.println(
+                "*** 영단어 마스터 ***\n" +
+                "*****************\n" +
+                "1. 모든 단어 보기\n" +
+                "2. 수준별 단어 보기\n" +
+                "3. 단어 검색\n" +
+                "4. 단어 추가\n" +
+                "5. 단어 수정\n" +
+                "6. 단어 삭제\n" +
+                "7. 파일 저장\n" +
+                "0. 나가기\n" +
+                "*****************"
+        );
+
+        return s.nextInt();
+    }
     public void start() {
-        System.out.println("Word Manager Started!!");
+        while(true){
+            int menu = selectMenu();
+            if(menu == 0) break;
+            if(menu == 4) {
+                wordCRUD.addWord();
+            }
+            else if(menu == 1) {
+                // list
+            }
+        }
+
     }
 
 }
