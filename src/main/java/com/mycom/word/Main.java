839b631f6b8d1bb5a79e341fdca0a8288fa093c5
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-09-03 16:14:54 +0900

    Create Class and Interface files

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -3,6 +3,6 @@ package com.mycom.word;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!! 안녕");
+        new WordManager().start();
     }
 }
---
+++ src/main/java/com/mycom/word/WordManager.java
@@ -0,0 +1,10 @@
+package com.mycom.word;
+
+public class WordManager {
+    // 실제 word를 관리하는 클래스
+
+    public void start() {
+        System.out.println("Word Manager Started!!");
+    }
+
+}
